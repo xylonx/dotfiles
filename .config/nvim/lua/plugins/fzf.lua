cd69bb9cf49f7de14e49ea4d5c2190c0022ffcb1
xylonx <x956200361@gmail.com>
2025-12-06 18:32:06 +0800

    :sparkles: (nvim) ignore some files for fzf

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,13 +1,25 @@
 return {
-  "ibhagwan/fzf-lua",
+  'ibhagwan/fzf-lua',
   -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "nvim-mini/mini.icons" },
   ---@module "fzf-lua"
   ---@type fzf-lua.Config|{}
   ---@diagnostics disable: missing-fields
   opts = {
+    file_ignore_patterns = {
+      'node_modules',
+      'dist',
+      '.next',
+      '.git',
+      '.gitlab',
+      'build',
+      'target',
+      'package-lock.json',
+      'pnpm-lock.yaml',
+      'yarn.lock',
+    },
   },
   ---@diagnostics enable: missing-fields
 }
