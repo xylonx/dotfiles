1074d590d0b091432cd7f3bf3c4c5185ab67ab80
xylonx <x956200361@gmail.com>
2025-12-06 18:27:00 +0800

    :sparkles: (nvim) add nvim plugins

--- .config/nvim/lua/plugins/code-formatter.lua
+++ .config/nvim/lua/plugins/code-formatter.lua
@@ -1,32 +1,57 @@
 -- Format code on save
+-- use conform instead as it is better
 return {
-  'mhartington/formatter.nvim',
-  config = function()
-    -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-    require('formatter').setup({
-      logging = true,
-      log_level = vim.log.levels.WARN,
-      -- All formatter configurations are opt-in
-      filetype = {
-        html = { require('formatter.filetypes.javascript').prettier },
-        javascript = { require('formatter.filetypes.javascript').prettier },
-        javascriptreact = { require('formatter.filetypes.javascript').prettier },
-        lua = { require('formatter.filetypes.lua').stylua },
-        go = { require('formatter.filetypes.go').goimports },
-        python = {
-          require('formatter.filetypes.python').isort,
-          require('formatter.filetypes.python').black,
-        },
-        rust = {
-          require('formatter.filetypes.rust').rustfmt,
-        },
-      },
-    })
+  'stevearc/conform.nvim',
+  opts = {
+    -- Conform will run multiple formatters sequentially
+    formatters_by_ft = {
+      -- Program language
+      lua = { 'stylua' },
+      rust = { 'rustfmt', lsp_format = 'fallback' },
+      javascript = { 'deno_fmt', 'prettierd', 'prettier', stop_after_first = true },
+      go = { 'goimports' },
+      python = { 'isort', 'black' },
 
-    vim.api.nvim_create_augroup('__formatter__', { clear = true })
-    vim.api.nvim_create_autocmd('BufWritePost', {
-      group = '__formatter__',
-      command = ':FormatWrite',
-    })
-  end,
+      -- File
+      toml = { 'taplo' },
+      json = { 'deno_fmt', 'prettierd', 'prettier', stop_after_first = true },
+      -- yaml = { 'deno_fmt', 'prettierd', 'prettier', stop_after_first = true },
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = 'fallback',
+    },
+  },
 }
+-- return {
+--   'mhartington/formatter.nvim',
+--   config = function()
+--     -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+--     require('formatter').setup({
+--       logging = true,
+--       log_level = vim.log.levels.WARN,
+--       -- All formatter configurations are opt-in
+--       filetype = {
+--         html = { require('formatter.filetypes.javascript').prettier },
+--         javascript = { require('formatter.filetypes.javascript').prettier },
+--         javascriptreact = { require('formatter.filetypes.javascript').prettier },
+--         lua = { require('formatter.filetypes.lua').stylua },
+--         go = { require('formatter.filetypes.go').goimports },
+--         python = {
+--           require('formatter.filetypes.python').isort,
+--           require('formatter.filetypes.python').black,
+--         },
+--         rust = {
+--           require('formatter.filetypes.rust').rustfmt,
+--         },
+--       },
+--     })
+--
+--     vim.api.nvim_create_augroup('__formatter__', { clear = true })
+--     vim.api.nvim_create_autocmd('BufWritePost', {
+--       group = '__formatter__',
+--       command = ':FormatWrite',
+--     })
+--   end,
+-- }
--- .config/nvim/lua/plugins/kitty-scrollback.lua
+++ .config/nvim/lua/plugins/kitty-scrollback.lua
@@ -1,12 +1,13 @@
 return {
-    'mikesmithgh/kitty-scrollback.nvim',
-    enabled = true,
-    lazy = true,
-    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
-    event = { 'User KittyScrollbackLaunch' },
-    -- version = '*', -- latest stable version, may have breaking changes if major version changed
-    -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-    config = function()
-      require('kitty-scrollback').setup()
-    end,
-  }
+  'mikesmithgh/kitty-scrollback.nvim',
+  enabled = true,
+  lazy = true,
+  version = '^6.0.0',
+  cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
+  event = { 'User KittyScrollbackLaunch' },
+  -- version = '*', -- latest stable version, may have breaking changes if major version changed
+  -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+  config = function()
+    require('kitty-scrollback').setup()
+  end,
+}
