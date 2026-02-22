8fca59bb4b09ec05bb5ef8ea0d76ed9587aae253
xylonx <xylonx@xylonx.com>
2026-02-22 20:36:52 +0800

    feat: conditionally install tools

--- init_archlinux.sh
+++ init_archlinux.sh
@@ -1,47 +1,62 @@
 #!/usr/bin/env bash
 
+function install_paru() {
+    if [ ! -x "$(command -v paru)" ]; then
+        local CUR_DIR="$PWD"
+        sudo pacman -S --needed base-devel
+        mkdir "/tmp/paru-install"
+        git clone https://aur.archlinux.org/paru.git
+        cd paru
+        makepkg -si
+        cd "$CUR_DIR"
+    fi
+}
+
 function install_tools() {
     for tool in "$@"; do
         if [ ! -x "$(command -v $tool)" ]; then
-            pacman -Syu $tool
+            sudo paru -Syu $tool
         else
             echo "$tool already exists"
         fi
     done
 }
 
-# Install basic tools
-install_tools git curl wget vim stow uv
+function install_cli_tools() {
+    # Install paru
+    install_paru
+    # Install basic tools
+    install_tools git curl wget vim stow uv
+    # Install tools for nvim plugins toolit
+    install_tools fzf fd ripgrep yazi mise neovim uutils-coreutils
+    # Install rust toolchains
+    install_tools rustup
+}
 
-# Install tools for nvim plugins toolit
-install_tools fzf fd ripgrep yazi mise neovim uutils-coreutils
+function install_shell() {
+    # Install oh-my-zsh
+    if [[ "$ZSH" != *"oh-my-zsh" ]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    else
+        echo "oh-my-zsh already exists"
+    fi
+}
 
-# Install useful cask apps
-install_tools kitty
+function install_gui_tools() {
+    install_tools kitty
+    install_tools firefox firefox-i18n-zh-cn
+}
 
-# Install Powerline Fonts
-# git clone https://github.com/powerline/fonts.git
-
-
-# Install oh-my-zsh
-if [[ "$ZSH" != *"oh-my-zsh" ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-    echo "oh-my-zsh already exists"
-fi
-
-
-# Install git commitizen
-# if [ ! -x "$(command -v cz)" ]; then
-#     uv tool install commitizen
-# else
-#     echo "commitizen already exists"
-# fi
-
-# Install rust toolchains
-if [ ! -x "$(command -v cargo)" ]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-else
-    echo "rust toolchain already exists"
-fi
+while getopts "c:g:" arg; do
+  case $arg in
+    c)
+      echo "Install Cli tools" 
+      install_cli_tools
+      ;;
+    g)
+      echo "Install GUI apps"
+      install_gui_tools
+      ;;
+  esac
+done
 
